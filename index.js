27375d3be1e391e0c65ffcbce3b85dfd45e089a3
Nathan Sainsbury <nathan.sainsbury@epas-ltd.com>
2018-02-07 13:02:19 +0000

    1

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var mysql = require('mysql');
-var http = require('http');
 var app = express();
 
 app.get('/:id', function(req,res){
+	
+	var id = req.params.id;
+	
 	res.send("Hello " + id);
 });
 
 console.log("hello");
 
-app.listen(3000);
+app.listen(2111);
