2836d5734cf2a9d92d4e4a741201a6a1a75b5882
Nathan Sainsbury <nathan.sainsbury@epas-ltd.com>
2018-02-07 13:50:15 +0000

    change

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var app = express();
 var http = require('http');
 
-http.createServer(function (req,res){
-	 res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);
+app.get('', function(req,res){
+	
+	res.send("HELLO");
+	
+});
